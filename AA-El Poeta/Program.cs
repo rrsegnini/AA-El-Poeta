48026d1ed7c2713fbcccb81c61e22918cf3c8340
rrsegnini <roberto.t.x@hotmail.com>
2017-11-14 01:32:33 -0600

    Diccionario

--- AA-El Poeta/Program.cs
+++ AA-El Poeta/Program.cs
@@ -8,8 +8,107 @@ namespace AA_El_Poeta
 {
     class Program
     {
+        public static IEnumerable<string> makeNgrams(string text, int nGramSize)
+        {
+            if (nGramSize == 0) throw new Exception("nGram size was not set");
+
+            StringBuilder nGram = new StringBuilder();
+            Queue<int> wordLengths = new Queue<int>();
+
+            int wordCount = 0;
+            int lastWordLen = 0;
+
+            //append the first character, if valid.
+            //avoids if statement for each for loop to check i==0 for before and after vars.
+            if (text != "" && char.IsLetterOrDigit(text[0]))
+            {
+                nGram.Append(text[0]);
+                lastWordLen++;
+            }
+
+            //generate ngrams
+            for (int i = 1; i < text.Length - 1; i++)
+            {
+                char before = text[i - 1];
+                char after = text[i + 1];
+
+                if (char.IsLetterOrDigit(text[i])
+                    ||
+                    //keep all punctuation that is surrounded by letters or numbers on both sides.
+                    (text[i] != ' '
+                        && (char.IsSeparator(text[i]) || char.IsPunctuation(text[i]))
+                        && (char.IsLetterOrDigit(before) && char.IsLetterOrDigit(after))
+                        )
+                    )
+                {
+                    nGram.Append(text[i]);
+                    lastWordLen++;
+                }
+                else
+                {
+                    if (lastWordLen > 0)
+                    {
+                        wordLengths.Enqueue(lastWordLen);
+                        lastWordLen = 0;
+                        wordCount++;
+
+                        if (wordCount >= nGramSize)
+                        {
+                            yield return nGram.ToString();
+                            nGram.Remove(0, wordLengths.Dequeue() + 1);
+                            wordCount -= 1;
+                        }
+
+                        nGram.Append(" ");
+                    }
+                }
+            }
+            nGram.Append(text.Last());
+            yield return nGram.ToString();
+        }
+
+
+
+
+        public static Dictionary<string, int> createDictionary(IEnumerable<string> ngram)
+        {
+            ngram.GetEnumerator();
+
+            Dictionary<string, int> dictionary =
+            new Dictionary<string, int>();
+
+            int i = 1;
+            foreach (var item in ngram)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    i = dictionary[item];
+                    i++;
+                    dictionary[item] = i;
+                    //dictionary.Add(item, i);
+                }
+                else
+                {
+                    dictionary.Add(item, 1);
+                }
+                //Console.WriteLine(item);  
+            }
+
+            return dictionary;
+        }
         static void Main(string[] args)
         {
+            IEnumerable<string> ngram = Program.makeNgrams("Hola mi nombre es Daniel Alvarado. Hola, mi nombre es Roberto Rojas Segnini", 2);
+            Dictionary<string, int> dictionary = createDictionary(ngram);
+            string output = String.Join(",", ngram);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+                //Console.WriteLine(output);
+                Console.ReadLine();
         }
     }
 }
