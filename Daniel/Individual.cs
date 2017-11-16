f593110444c1ed0a113f9c9ef8b0709fe1a02631
danielalvarado <danialvarado@ic-itcr.ac.cr>
2017-11-15 20:02:24 -0600

    Intento de copiar el del video, faltan cosas

---
+++ Daniel/Individual.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace AA_PROJECT3
+{
+    public class Individual
+    {
+        //public string[] genes;
+        public string[] genes;
+        public float fitness{ get; private set; }
+
+        private Func<int, float> fitnessFunction;
+        private Random random;
+        private Func<string> getRandomGene;
+
+        public Individual(int size, Random random, Func<string> getRandomGene, Func<int, float> fitnessFunction, bool shouldInitGenes = true)
+        {
+            genes = new string[size];
+            this.random = random;
+            this.getRandomGene = getRandomGene;
+            this.fitnessFunction = fitnessFunction;
+
+            if (shouldInitGenes)
+            {
+                for (int i = 0; i < genes.Length; i++)
+                {
+                    genes[i] = getRandomGene();
+                }
+            }
+
+        }
+
+        public float CalculateFitness(int index)
+        {
+            fitness = fitnessFunction(index);
+            return fitness;
+        }
+
+        public void Mutate(float mutationRate)
+        {
+            for (int i = 0; i < genes.Length; i++)
+            {
+                if (random.NextDouble() < mutationRate)
+                {
+                    genes[i] = getRandomGene();
+                }
+            }
+        }
+    }
+}
---
+++ Daniel/Poem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace AA_PROJECT3
+{
+    public class Poem
+    {
+
+        string targetString = "Hola, soy conejos";
+        string data = "Hola, soy Roberto Rojas Segnini. Me gustan los conejos";
+        int populationSize = 200;
+        float mutationRate = 0.01f;
+        int elitism = 5;
+
+
+        int numGenerations;
+        private GeneticAlgorithm<char> ga;
+        private System.Random random;
+
+        void Start()
+        {
+
+            random = new System.Random();
+            ga = new GeneticAlgorithm<char>(populationSize, targetString.Length, random, GetRandomString, FitnessFunction, elitism, mutationRate);
+        }
+
+        void Update()
+        {
+            ga.NewGeneration();
+
+            //UpdateText(ga.BestGenes, ga.BestFitness, ga.Generation, ga.Population.Count, (j) => ga.Population[j].Genes);
+
+
+        }
+
+        public static int GetAmountOfElements(IEnumerable<string> ngram) {
+            int counter = 0;
+            foreach (var item in ngram)
+            {
+                counter++;
+
+            }
+            return counter;
+        }
+
+        public static string[] createStringList(IEnumerable<string> ngram)
+        {
+            int length = Poem.GetAmountOfElements(ngram);
+            List<string> dictionary = new List<string>();
+
+            foreach (var item in ngram)
+            {
+                dictionary.Add(item);
+
+            }
+
+            return dictionary.ToArray();
+
+        }
+
+
+        private string GetRandomString()
+        {
+            int i = random.Next(validCharacters.Length);
+            return validCharacters[i];
+        }
+
+        private float FitnessFunction(int index)
+        {
+            float score = 0;
+            DNA<char> dna = ga.Population[index];
+
+            for (int i = 0; i < dna.Genes.Length; i++)
+            {
+                if (dna.Genes[i] == targetString[i])
+                {
+                    score += 1;
+                }
+            }
+
+            score /= targetString.Length;
+
+            score = (Mathf.Pow(2, score) - 1) / (2 - 1);
+
+            return score;
+        }
+
+    }
+}
