a08851177f146287415da328ba5eec73d07eaa32
rrsegnini <roberto.t.x@hotmail.com>
2017-11-15 23:49:45 -0600

    Más cositas

--- AA-El Poeta/Program.cs
+++ AA-El Poeta/Program.cs
@@ -11,7 +11,7 @@ namespace AA_El_Poeta
     class Program
     {
         static int Generations = 0;
-
+        static Random rand = new Random();
         static string poema20 = "Puedo escribir los versos más tristes esta noche.Escribir, por ejemplo: «La noche está estrellada," +
                 "y tiritan, azules, los astros, a lo lejos.» " +
                 "El viento de la noche gira en el cielo y canta." +
@@ -137,31 +137,35 @@ namespace AA_El_Poeta
             foreach (DictionaryEntry item in datos)
             {
                 dictionary.Add(item.Key, item.Value);
-                Console.WriteLine(item.Key + " " + item.Value);
+                //
+                //Console.WriteLine(item.Key + " " + item.Value);
             }
         }
         public static OrderedDictionary CreateOrderedDictionary(OrderedDictionary datos, IEnumerable<string> ngram)
 
         {
-            ngram.GetEnumerator();
+
+
             OrderedDictionary dictionary = new OrderedDictionary();
-            DeepCopy(datos, dictionary);
-
-            
-
-            /*OrderedDictionary dictionary =
-            new OrderedDictionary();*/
+            //DeepCopy(datos, dictionary);
 
             int i = 1;
-            
+
             foreach (var item in ngram)
             {
-                //dictionary.Contains
-                if (dictionary.Contains(item))
+                foreach (DictionaryEntry item2 in datos)
+                {
+                    if ((string)item2.Value == item)
+                    {
+                        dictionary.Add(item2.Key, item2.Value);
+                    }
+                }
+                /*
+                if (datos.Contains(item))
                 {
 
-                    i = (int)dictionary[item];
-                    
+                    i = (int)datos[item];
+
                     i++;
                     dictionary[item] = i;
                     //dictionary.Add(item, i);
@@ -171,9 +175,42 @@ namespace AA_El_Poeta
                     //dictionary.Add(item, 1);
                 }
                 //Console.WriteLine(item);  
+            }*/
             }
+            //PrintDictionary(dictionary);
+                return dictionary;
 
-            return dictionary;
+                /*
+                OrderedDictionary dictionary = new OrderedDictionary();
+                DeepCopy(datos, dictionary);
+
+
+
+
+
+                int i = 1;
+
+                foreach (var item in ngram)
+                {
+                    //dictionary.Contains
+                    if (dictionary.Contains(item))
+                    {
+
+                        i = (int)dictionary[item];
+
+                        i++;
+                        dictionary[item] = i;
+                        //dictionary.Add(item, i);
+                    }
+                    else
+                    {
+                        //dictionary.Add(item, 1);
+                    }
+                    //Console.WriteLine(item);  
+                }
+
+                return dictionary;*/
+           
         }
 
         //public static void Manhattan(Dictionary<string, int> dictionaryGoal, Dictionary<string, int> dictionary)
@@ -245,11 +282,11 @@ namespace AA_El_Poeta
         {
             string output = "";
             
-            Random rand = new Random();
-            int number = rand.Next(1, 43);
+            
+            //int number = rand.Next(1, 43);
 
 
-            var r = new Random();
+            //var r = new Random();
         
 
 
@@ -259,11 +296,11 @@ namespace AA_El_Poeta
             int num = 0;
             foreach (DictionaryEntry item in datos)
             {
-                int YesNo = rand.Next(0, 5);
+                int YesNo = rand.Next(0, 3);
                 //Console.WriteLine(YesNo);
                 if (YesNo == 1)
                 {
-                    resultado.Add(item.Key, 1);
+                    resultado.Add(item.Key, item.Value);
 
                     num++;
                     if (num == size)
@@ -274,7 +311,7 @@ namespace AA_El_Poeta
                 }
                 else
                 {
-                    resultado.Add(item.Key, 0);
+                    //resultado.Add(item.Key, 0);
                 }
             }
 
@@ -290,7 +327,25 @@ namespace AA_El_Poeta
         public static OrderedDictionary CreateDictionaryZero(IEnumerable<string> ngram)
         {
 
+
             OrderedDictionary dictionary =
+            new OrderedDictionary();
+
+            int i = 1;
+
+            foreach (var item in ngram)
+            {
+                if (!dictionary.Contains(item))
+                {
+                    dictionary.Add(i, item);
+                    i++;
+                }
+
+            }
+            //PrintDictionary(dictionary);
+            return dictionary;
+
+            /*OrderedDictionary dictionary =
             new OrderedDictionary();
 
             int i = 1;
@@ -304,8 +359,8 @@ namespace AA_El_Poeta
                   
             }
 
-            return dictionary;
-            
+            return dictionary;*/
+
         }
 
 
@@ -360,13 +415,13 @@ namespace AA_El_Poeta
             foreach (var item in normalOrderedDictionary)
             {
                 orderedFinal.Add(item.Key, item.Value);
-                Console.WriteLine(item.Key + " " + item.Value);
+                //Console.WriteLine(item.Key + " " + item.Value);
             }
 
             foreach (DictionaryEntry item in orderedFinal)
             {
                 
-                Console.WriteLine(item.Key + " " + item.Value);
+                //Console.WriteLine(item.Key + " " + item.Value);
             }
 
 
@@ -385,14 +440,14 @@ namespace AA_El_Poeta
             OrderedDictionary individuo = new OrderedDictionary();
             for (int i = 0; i < 100; i++)
             {
-                individuo = GetPoem(datos, poemaMeta.Count);
-                PrintDictionary(individuo);
+                individuo = GetPoem(datos, 10);
+                //PrintDictionary(individuo);
                 //individuo.
                 generation.Add(i, individuo);
             }
 
             Generations++;
-            Console.WriteLine("Generaciones: " + Generations);
+            //Console.WriteLine("Generaciones: " + Generations);
             return generation;
         }
 
@@ -436,15 +491,22 @@ namespace AA_El_Poeta
             foreach (var item in normalOrderedDictionary)
             {
                 orderedFinal.Add(item.Key, item.Value);
-                Console.WriteLine(item.Key + " " + item.Value);
-                PrintDictionary((OrderedDictionary)item.Value);
+                //Console.WriteLine(item.Key + " " + item.Value);
+                //PrintDictionary((OrderedDictionary)item.Value);
             }
 
-               PrintDictionary (Crossover(orderedFinal));
+               //PrintDictionary (Crossover(orderedFinal));
             return Crossover(orderedFinal);
+            //return orderedFinal;
 
         }
 
+
+
+        public static OrderedDictionary GetBestElement(OrderedDictionary dict)
+        {
+            return (OrderedDictionary)dict[0];
+        }
 
 
         public static void PrintDictionary(OrderedDictionary dict)
@@ -465,6 +527,57 @@ namespace AA_El_Poeta
             }
 
             return result;
+        }
+
+        public static OrderedDictionary Genetic3(OrderedDictionary datos, OrderedDictionary poemaMeta, OrderedDictionary ElementosPrometedores)
+        {
+            OrderedDictionary generation = new OrderedDictionary();
+
+            if (ElementosPrometedores == null)
+            {
+                generation = CreateGeneration(datos, poemaMeta);
+                
+            }
+            else
+            {
+                //generation = new OrderedDictionary();
+                DeepCopy(ElementosPrometedores, generation);
+            }
+
+
+
+            int distance = 0;
+            OrderedDictionary ordered = new OrderedDictionary();
+            for (int i = 0; i < generation.Count; i++)
+
+            {
+
+                distance = CompareDict(poemaMeta, (OrderedDictionary)generation[i]);
+                //Console.WriteLine(distance);
+
+                if (!ordered.Contains(distance))
+                {
+                    ordered.Add(distance, (OrderedDictionary)generation[i]);
+                }
+
+            }
+
+            var normalOrderedDictionary = ordered.Cast<DictionaryEntry>()
+                      .OrderBy(r => r.Key)
+                      .ToDictionary(c => c.Key, d => d.Value);
+
+            OrderedDictionary orderedFinal = new OrderedDictionary();
+            foreach (var item in normalOrderedDictionary)
+            {
+                orderedFinal.Add(item.Key, item.Value);
+                //Console.WriteLine(item.Key + " " + item.Value);
+                //PrintDictionary((OrderedDictionary)item.Value);
+            }
+
+            //PrintDictionary((orderedFinal));
+            //return (orderedFinal);
+            return orderedFinal;
+
         }
 
 
@@ -497,21 +610,77 @@ namespace AA_El_Poeta
             OrderedDictionary NewElements = new OrderedDictionary();
             foreach (DictionaryEntry item in population)
             {
-                if (first)
+                /* if (first)
+                 {
+                     first = false;
+                 }
+                 else
+                 {
+                     NewElements.Add(cont, SumDictionaries(prev, (OrderedDictionary)item.Value));
+                     cont++;
+                 }
+                 prev = (OrderedDictionary)item.Value;*/
+
+                foreach (DictionaryEntry item2 in (OrderedDictionary)item.Value)
                 {
-                    first = false;
+                    if ((int)item2.Value == 1)
+                    {
+                        //Console.WriteLine(item2.Key);
+                    }
                 }
-                else
-                {
-                    NewElements.Add(cont, SumDictionaries(prev, (OrderedDictionary)item.Value));
-                    cont++;
-                }
-                prev = (OrderedDictionary)item.Value;
+
             }
 
             return NewElements;
         }
 
+        
+
+
+        public static OrderedDictionary NewGroup(OrderedDictionary datos, OrderedDictionary dict, OrderedDictionary poemaMeta)
+        {
+            OrderedDictionary generation = new OrderedDictionary();
+            OrderedDictionary individuo = new OrderedDictionary();
+            OrderedDictionary bestGen = new OrderedDictionary();
+
+            int cont = 30;
+            foreach (DictionaryEntry item in dict)
+            {
+                if (cont != 0)
+                {
+                    bestGen.Add(item.Key, item.Value);
+                    cont--;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                individuo = GetPoem(bestGen, poemaMeta.Count / 8);
+                //PrintDictionary(individuo);
+                //individuo.
+                generation.Add(i, individuo);
+            }
+
+            /*for (int i = 80; i < 80; i++)
+            {
+                individuo = GetPoem(datos, poemaMeta.Count / 8);
+                //PrintDictionary(individuo);
+                //individuo.
+                generation.Add(i, individuo);
+            }*/
+
+            Generations++;
+            //Console.WriteLine("Generaciones: " + Generations);
+            //PrintDictionary(generation);
+            return generation;
+
+            //return CreateGeneration(dict, poemaMeta) + CreateGeneration(datos, poemaMeta);
+        }
 
         public static int CompareDictionaries(OrderedDictionary dict1, OrderedDictionary dict2)
         {
@@ -528,53 +697,101 @@ namespace AA_El_Poeta
 
             return distance;
         }
-    
-        
 
+
+        public static int CompareDict(OrderedDictionary dict1, OrderedDictionary dict2)
+        {
+            int id_1 = 0;
+            int id_2 = 0;
+            int resultado = 0;
+            foreach (DictionaryEntry item in dict1)
+            {
+                id_1 += (int)item.Key;
+            }
+            foreach (DictionaryEntry item in dict2)
+            {
+                id_2 += (int)item.Key;
+            }
+
+            resultado = CalculateManhattanDistance(id_1, id_2);
+
+            return resultado;
+        }
 
         static void Main(string[] args)
         {
             //string datos = readFile("C:/Users/CASA/source/repos/AA-El Poeta/all.txt");
             string datos = "Hola, soy Roberto Rojas Segnini. Me gustan los conejos soy Carlos amo los perros";
+            //IEnumerable<string> ngramDatos1 = Program.makeNgrams(datos, 1);
             IEnumerable<string> ngramDatos2 = Program.makeNgrams(datos, 2);
+            IEnumerable<string> ngramDatos3 = Program.makeNgrams(datos, 3);
+            IEnumerable<string> ngramDatos4 = Program.makeNgrams(datos, 4);
             string outputDatos = String.Join("|||", ngramDatos2);
-            Console.WriteLine(outputDatos);
+           // Console.WriteLine(outputDatos);
 
-            OrderedDictionary dictDatos2 = CreateDictionaryZero(ngramDatos2);
+            OrderedDictionary dictDatos2 = CreateDictionaryZero(ngramDatos2.Concat(ngramDatos3).Concat(ngramDatos4));
 
 
-            string meta = "Hola soy Roberto";
+            //string meta = "Here take my picture; though I bid farewell";
+            string meta = "Hola me gustan los conejos";
             IEnumerable<string> ngramMeta = Program.makeNgrams(meta, 2);
             string outputMeta = String.Join("|||", ngramMeta);
-            Console.WriteLine(outputMeta);
+            //Console.WriteLine(outputMeta);
             OrderedDictionary dictMeta = CreateOrderedDictionary(dictDatos2, ngramMeta);
 
+            int distanciaBase = CompareDict(dictMeta, dictMeta);
 
-
-            string poema = "Hola, me gustan los conejos. Soy Roberto Rojas Segnini";
+            string poema = "I love you, man. Girl world peace";
             IEnumerable<string> ngramPoema2 = Program.makeNgrams(poema, 2);
             string output = String.Join("|||", ngramPoema2);
 
             OrderedDictionary dict = CreateOrderedDictionary(dictDatos2, ngramPoema2);
 
-            foreach (DictionaryEntry item in dict)
+            Console.WriteLine(CompareDict(dictMeta, dict));
+
+            //PrintDictionary(GetPoem(dictDatos2, ngramMeta.Count()));
+
+            OrderedDictionary a =
+            Genetic3(dictDatos2, dictMeta, null);
+            OrderedDictionary datos2 = dictDatos2;
+            int fin = 100;
+            for (int i = 0; i < fin; i++)
             {
-               // Console.WriteLine(item.Key + "  " + item.Value);
+                a = Genetic3(datos2, dictMeta, a);
+                if (i != fin - 1)
+                {
+                    //Console.WriteLine(i);
+                    a = NewGroup(dictDatos2, a, dictMeta);
+                }
+                datos2 = NewGroup(dictDatos2, a, dictMeta);
             }
 
+            //PrintDictionary(a);
+            //PrintDictionary(GetBestElement(a));
 
-            //Console.WriteLine(CompareDictionaries(dictMeta, dict));
-            //Console.WriteLine(output);
+            /*
+            
 
 
-            Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, null))));
+            //Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, Genetic2(dictDatos2, dictMeta, null))));
 
-
-            /*foreach (DictionaryEntry item in Genetic(dictDatos2, dictMeta, new OrderedDictionary()))
+            
+            for (int i = 0; i < 1; i++)
             {
-                Console.WriteLine(item.Key);
-            }*/
-            //Console.WriteLine(Genetic(poema20));
+                a = Genetic2(dictDatos2, dictMeta, a);
+            }
+
+            a = Genetic2(dictDatos2, dictMeta, a);
+            foreach (DictionaryEntry item in a)
+            {
+                Console.WriteLine("Hola");
+                //if ((int)item.Value == 0) { Console.WriteLine(item.Key); }
+                   
+            }
+
+           
+
+            //Console.WriteLine(Genetic(poema20));*/
             Console.ReadLine();
         }
     }
